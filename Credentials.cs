09a716d93434fa6b724d595e6a227b6ce76f3963
Nicclassy <lucanapoli122@gmail.com>
2025-01-06 12:10:47 +1100

    Restructure program

---
+++ Credentials.cs
@@ -0,0 +1,36 @@
+﻿using DotNetEnv;
+
+namespace Autogrator;
+
+public static class Credentials {
+    static Credentials() {
+        Env.Load(EnvPath());
+    }
+    public static class Outlook {
+        public static readonly string Email = EnvironmentVariable("AG_EMAIL");
+        public static readonly string Password = EnvironmentVariable("AG_PASSWORD");
+    }
+
+    private static string EnvPath() {
+        string path = ".env";
+        string dir = Directory.GetCurrentDirectory();
+        string? file = Path.GetFileName(path);
+        path = Path.Combine(dir, file);
+
+        while (!File.Exists(path)) {
+            var parent =
+                Directory.GetParent(dir) ?? throw new ApplicationException(".env not found");
+            dir = parent.FullName;
+            path = Path.Combine(dir, file);
+        }
+
+        return path;
+    }
+
+    private static string EnvironmentVariable(string name) {
+        string? value = Environment.GetEnvironmentVariable(name);
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException($"Environment variable ${name} not found.");
+        return value;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,23 @@
-﻿using DotNetEnv;
-using Microsoft.Extensions.Logging;
-using Outlook = Microsoft.Office.Interop.Outlook;
+﻿using Serilog;
 
-namespace EmailAutomation;
+using Autogrator.OutlookAutomation;
 
-public sealed class EmailAutomationTest
-{
-    private static readonly string EMAIL_ADDRESS;
-    private static readonly string PASSWORD;
-    private static readonly ILogger<EmailAutomationTest> logger;
+namespace Autogrator;
 
-    static EmailAutomationTest()
-    {
-        using var factory = LoggerFactory.Create(builder => builder.AddConsole());
-        logger = factory.CreateLogger<EmailAutomationTest>();
-
-        Env.Load(".env");
-        EMAIL_ADDRESS = Environment.GetEnvironmentVariable("AG_EMAIL")!;
-        PASSWORD = Environment.GetEnvironmentVariable("AG_PASSWORD")!;
+public static class Program {
+    static Program() {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+        Log.Information("Begin logging...");
     }
-
-    public void ReadOutlookMail()
-    {
-        logger.LogInformation("Reading emails...");
-
-        Outlook.Application outlookApp = new Outlook.Application();
-        Outlook.NameSpace outlookNamespace = outlookApp.GetNamespace("MAPI");
-        logger.LogInformation($"Logging in with email ${EMAIL_ADDRESS}...");
-        outlookNamespace.Logon(EMAIL_ADDRESS, PASSWORD, ShowDialog: false, NewSession: true);
-        logger.LogInformation("Successfully logged in!");
-
-        Outlook.MAPIFolder inbox = outlookNamespace.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
-        Outlook.Items mailItems = inbox.Items;
-        foreach (Outlook.MailItem mailItem in mailItems.OfType<Outlook.MailItem>())
-            Console.WriteLine("Email subject: " + mailItem.Subject);
-
-        outlookNamespace.Logoff();
-        logger.LogInformation("Logged off.");
-    }
-}
-
-public sealed class Program
-{
-    public static void Main()
-    {
-        var emailAutomationTest = new EmailAutomationTest();
-        emailAutomationTest.ReadOutlookMail();
+    public static void Main() {
+       var receiver = OutlookEmailReceiver.Create();
+       if (receiver.TryReceiveEmail(out var email)) {
+            Log.Information($"Received information: {email}");
+       } else {
+            Log.Information("Did not receive anything.");
+       }
+       Log.CloseAndFlush();
     }
 }
