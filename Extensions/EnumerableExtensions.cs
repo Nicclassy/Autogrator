586f2fe52486f17edd346fa462fe5052055edc5f
Nicclassy <lucanapoli122@gmail.com>
2025-01-09 17:42:18 +1100

    Refactor code into different namespaces and add extensions

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Autogrator.Extensions;
+
+public static class EnumerableExtensions {
+    public static void Print<T>(
+        this IEnumerable<T> source, 
+        string delimiter = ", ", 
+        Func<T, string>? formatter = null
+    ) where T: notnull {
+        string FormatValue(T value) => $"\"{formatter?.Invoke(value) ?? value.ToString()}\"";
+
+        if (!source.Any()) {
+            Console.WriteLine();
+            return;
+        }
+
+        Console.Write('[');
+        Console.Write(FormatValue(source.First()));
+
+        foreach (T value in source.Skip(1)) {
+            Console.Write(delimiter);
+            Console.Write(FormatValue(value));
+        }
+
+        Console.Write(']');
+        Console.WriteLine();
+    }
+}
--- Extensions/OutlookInteropExtensions.cs
+++ Extensions/OutlookInteropExtensions.cs
@@ -5,9 +5,12 @@ using Outlook = Microsoft.Office.Interop.Outlook;
 namespace Autogrator.Extensions;
 
 public static class OutlookInteropExtensions {
-    public static IEnumerable<Outlook.MailItem> MailItems(this Outlook.MAPIFolder folder) =>
+    public static IEnumerable<Outlook.MailItem> Emails(this Outlook.MAPIFolder folder) =>
         folder.Items.OfType<Outlook.MailItem>();
 
-    public static IEnumerable<Outlook.MailItem> EmailsByLatest(this Outlook.MAPIFolder source) =>
-        source.Items.OfType<Outlook.MailItem>().OrderBy(item => item.ReceivedTime).Reverse();
+    public static IEnumerable<Outlook.MailItem> EmailsByLatest(this Outlook.MAPIFolder folder) =>
+        folder.Items.OfType<Outlook.MailItem>().OrderBy(item => item.ReceivedTime).Reverse();
+
+    public static Outlook.MailItem? LatestEmail(this Outlook.MAPIFolder folder) =>
+        folder.EmailsByLatest().FirstOrDefault();
 }
