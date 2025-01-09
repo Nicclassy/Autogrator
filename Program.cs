e622280acfdee16dfa99874bc531d475f75e33a4
Nicclassy <lucanapoli122@gmail.com>
2025-01-09 17:52:46 +1100

    Add Microsoft Graph integration via `AutogratorApplicationClient`

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Serilog;
 
 using Autogrator.OutlookAutomation;
 using Autogrator.Extensions;
+using Autogrator.Helpers;
 
 namespace Autogrator;
 
@@ -27,10 +28,39 @@ public static class Program {
 
     public static void ExportFirstEmail() {
         using OutlookEmailReceiver receiver = OutlookEmailReceiver.Create();
-        List<Outlook.MailItem> emails = receiver.Inbox.MailItems().ToList();
-        
+        List<Outlook.MailItem> emails = receiver.Inbox.Emails().ToList();
+
         var email = emails[0];
         OutlookEmailExporter.SaveAndExportEmail(email);
+    }
+
+    public static void PrintSenderAndRecipients() {
+        using OutlookEmailReceiver receiver = OutlookEmailReceiver.Create();
+        Outlook.MailItem email = receiver.Inbox.EmailsByLatest().ElementAt(4);
+        Console.WriteLine(email.SenderEmailAddress);
+        Console.WriteLine(email.SenderName);
+        Console.WriteLine(email.ReplyRecipientNames);
+        Console.WriteLine();
+
+        email.Recipients.OfType<Outlook.Recipient>().Print(formatter: recipient => recipient.Address);
+        Console.Write("Sender: ");
+        Console.WriteLine(email.Sender.Name);
+
+        Console.WriteLine(EmailHelper.GetEmailAddressDomain("person@domain.com"));
+    }
+
+    public static void PrintEmailAddresses() {
+        using OutlookEmailReceiver receiver = OutlookEmailReceiver.Create();
+        foreach (Outlook.MailItem mailItem in receiver.Inbox.EmailsByLatest()) {
+            if (EmailHelper.IsEmailAddress(mailItem.SenderEmailAddress))
+                Console.WriteLine(mailItem.SenderEmailAddress);
+        }
+    }
+
+    public static void PrintRootSiteID() {
+        using AutogratorApplicationClient client = new();
+        var result = client.GetRootSite().Result;
+        Console.WriteLine(result!.Id);
     }
 
     public static void Main() {}
---
+++ SharePointAutomation/AutogratorApplicationClient.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+using Serilog;
+
+using Autogrator.Utilities;
+
+namespace Autogrator.Helpers;
+
+public sealed class AutogratorApplicationClient(GraphServiceClient graphClient) : IDisposable {
+    private static readonly string[] Scopes = { AutogratorApplication.Scope };
+
+    public GraphServiceClient GraphClient { get; } = graphClient;
+
+    public AutogratorApplicationClient() : this(CreateGraphClient()) {}
+
+    public async Task<Microsoft.Graph.Models.Site?> GetRootSite() =>
+        await GraphClient.Sites[Sites.Hostname].GetAsync();
+
+    private static GraphServiceClient CreateGraphClient() {
+        var options = new TokenCredentialOptions {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+        };
+
+        ClientSecretCredential tokenCredential = new(
+            AutogratorApplication.TenantID,
+            AutogratorApplication.ClientID,
+            AutogratorApplication.ClientSecret,
+            options
+        );
+
+        return new(tokenCredential, Scopes);
+    }
+
+    public void Dispose() => GraphClient.Dispose();
+}
