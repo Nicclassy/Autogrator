c28bd480876cc0e8b09c37945630e313c7ddba55
Nicclassy <lucanapoli122@gmail.com>
2025-01-10 09:43:49 +1100

    Integrate code with the Autogrator app registration/service principal

--- SharePointAutomation/AutogratorApplicationClient.cs
+++ SharePointAutomation/AutogratorApplicationClient.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.Graph;
 using Azure.Identity;
-using Serilog;
+using Graph = Microsoft.Graph.Models;
 
 using Autogrator.Utilities;
+using Serilog;
 
-namespace Autogrator.Helpers;
+namespace Autogrator.SharePointAutomation;
 
 public sealed class AutogratorApplicationClient(GraphServiceClient graphClient) : IDisposable {
     private static readonly string[] Scopes = { AutogratorApplication.Scope };
@@ -13,8 +14,26 @@ public sealed class AutogratorApplicationClient(GraphServiceClient graphClient) 
 
     public AutogratorApplicationClient() : this(CreateGraphClient()) {}
 
-    public async Task<Microsoft.Graph.Models.Site?> GetRootSite() =>
+    public async Task<Graph.Site?> GetRootSiteAsync() =>
         await GraphClient.Sites[Sites.Hostname].GetAsync();
+
+    public async Task<Graph.Drive?> GetRootDriveAsync() {
+        Graph.Site? rootSite = await GetRootSiteAsync();
+        return rootSite is Graph.Site { Id: string id } ? 
+            await GraphClient.Sites[id].Drive.GetAsync() : null;
+    }
+
+    public async Task<Graph.DriveItem?> GetSiteAtPathAsync(string sitePath) {
+        Graph.Drive? rootDrive = await GetRootDriveAsync();
+        Log.Information($"Root drive name is {rootDrive?.Name} and is {rootDrive?.Id}");
+        return rootDrive is Graph.Drive { Id: string id } ?
+            await GraphClient.Drives[rootDrive.Id].Root.ItemWithPath(sitePath).GetAsync() : null;
+    }
+
+    public async Task<List<Graph.DriveItem>?> GetSiteSubfoldersAsync(string sitePath) {
+        Graph.DriveItem? siteDrive = await GetSiteAtPathAsync(sitePath);
+        return siteDrive?.Children;
+    }
 
     private static GraphServiceClient CreateGraphClient() {
         var options = new TokenCredentialOptions {
@@ -28,6 +47,7 @@ public sealed class AutogratorApplicationClient(GraphServiceClient graphClient) 
             options
         );
 
+        Log.Information("Created application client");
         return new(tokenCredential, Scopes);
     }
 
