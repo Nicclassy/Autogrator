5c0379f245c2359ca0191562b5aaae19607fdc88
Nicclassy <lucanapoli122@gmail.com>
2025-01-16 22:55:40 +1100

    Implement functionality for receiving paginated API responses

--- SharePointAutomation/GraphHttpClient.cs
+++ SharePointAutomation/GraphHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Text.Json;
+using System.Net.Http.Headers;
 
 using Newtonsoft.Json.Linq;
 using Serilog;
@@ -11,11 +12,12 @@ using Autogrator.Utilities;
 namespace Autogrator.SharePointAutomation;
 
 public sealed class GraphHttpClient(HttpClient httpClient) {
-    private static readonly Encoding PostEncoding = Encoding.UTF8;
     private const string PostMediaType = "application/json";
+    private const string PaginationKey = "@odata.nextLink";
+    private static readonly Encoding PostEncoding = Encoding.UTF8;
 
     internal async Task<string> GetKeyAsync(string endpoint, string key, CancellationToken cancellationToken) {
-        string content = await GetAsync($"{endpoint}?{FormatRequestKeys([key])}", cancellationToken);
+        string content = await GetAsync($"{endpoint}{FormatRequestKeys([key])}", cancellationToken);
         using JsonDocument document = JsonDocument.Parse(content);
         return document.RootElement.GetProperty(key).GetString()!;
     }
@@ -23,47 +25,72 @@ public sealed class GraphHttpClient(HttpClient httpClient) {
     internal async Task<IEnumerable<JToken>> GetValuesByKeysAsync(
         string endpoint, CancellationToken cancellationToken, params string[] keys
     ) {
-        string content = await GetAsync($"{endpoint}?{FormatRequestKeys(keys)}", cancellationToken);
-        JObject root = JObject.Parse(content);
-        return root["value"]!.Children();
+        string content = await GetAsync($"{endpoint}{FormatRequestKeys(keys)}", cancellationToken);
+        JObject json = JObject.Parse(content);
+        return json["value"]!.Children();
+    }
+
+    internal async Task<List<T>> GetPaginatedAsync<T>(
+        string endpoint, CancellationToken cancellationToken, params string[] keys
+    ) {
+        List<T> items = [];
+        string? currentEndpoint = $"{endpoint}{FormatRequestKeys(keys)}";
+        do {
+            string content = await GetAsync(currentEndpoint, cancellationToken);
+            JObject json = JObject.Parse(content);
+            IEnumerable<T> values = json["value"]!
+                .Children()
+                .Select(token => token.ToObject<T>()!);
+            items.AddRange(values);
+            currentEndpoint = json[PaginationKey]?.ToString();
+        } while (currentEndpoint is not null);
+
+        return items;
     }
 
     internal async Task<string> GetAsync(string endpoint, CancellationToken cancellationToken) {
         string requestUri = CreateRequestUri(endpoint);
-        HttpResponseMessage message = await httpClient.GetAsync(requestUri, cancellationToken);
-        if (!message.IsSuccessStatusCode)
-            LogFailureAndExit(endpoint, message);
+        HttpResponseMessage response = await httpClient.GetAsync(requestUri, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+            LogFailureAndExit("GET", endpoint, response);
 
-        return await message.Content.ReadAsStringAsync(cancellationToken);
+        return await response.Content.ReadAsStringAsync(cancellationToken);
     }
 
     internal async Task<string> PostAsync(string endpoint, string data, CancellationToken cancellationToken) {
         string requestUri = CreateRequestUri(endpoint);
         StringContent content = new(data, PostEncoding, PostMediaType);
-        HttpResponseMessage message = await httpClient.PostAsync(requestUri, content, cancellationToken);
-        if (!message.IsSuccessStatusCode)
-            LogFailureAndExit(endpoint, message);
+        HttpResponseMessage response = await httpClient.PostAsync(requestUri, content, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+            LogFailureAndExit("POST", endpoint, response);
 
-        return await message.Content.ReadAsStringAsync(cancellationToken);
+        return await response.Content.ReadAsStringAsync(cancellationToken);
     }
 
     internal async Task<string> PutAsync(string endpoint, HttpContent content, CancellationToken cancellationToken) {
         string requestUri = CreateRequestUri(endpoint);
-        HttpResponseMessage message = await httpClient.PostAsync(requestUri, content, cancellationToken);
-        if (!message.IsSuccessStatusCode)
-            LogFailureAndExit(endpoint, message);
+        HttpRequestMessage request = new(HttpMethod.Put, requestUri) {
+            Content = content,
+        };
+        request.Content!.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-        return await message.Content.ReadAsStringAsync(cancellationToken);
+        HttpResponseMessage response = await httpClient.SendAsync(request, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+            LogFailureAndExit("PUT", endpoint, response);
+
+        return await response.Content.ReadAsStringAsync(cancellationToken);
     }
 
-    private static void LogFailureAndExit(string endpoint, HttpResponseMessage message) {
-        string errorMessage = $"Request POST {endpoint} failed with status code {(int)message.StatusCode}. "
-                + $"Reason: {message.ReasonPhrase}";
+    private static void LogFailureAndExit(string method, string endpoint, HttpResponseMessage response) {
+        string errorMessage = $"Request {method} {endpoint} failed with status code {(int)response.StatusCode}. "
+            + $"Reason: {response.ReasonPhrase}";
         Log.Fatal(errorMessage.Colourise(AnsiColours.Red));
         throw new RequestUnsuccessfulException();
     }
 
-    private static string CreateRequestUri(string endpoint) => GraphAPI.URL + endpoint;
+    private static string CreateRequestUri(string endpoint) =>
+        endpoint.StartsWith("https") ? endpoint : GraphAPI.URL + endpoint;
 
-    private static string FormatRequestKeys(string[] keys, string query = "select") => $"${query}={string.Join(',', keys)}";
+    private static string FormatRequestKeys(string[] keys, string query = "select") =>
+        keys.Length > 0 ? $"?${query}={string.Join(',', keys)}" : "";
 }
