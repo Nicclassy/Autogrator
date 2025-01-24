ed47328be0a3ed3ef7b638d6ec970730a2f585d9
Nicclassy <lucanapoli122@gmail.com>
2025-01-25 09:31:56 +1100

    Add `HTMLBodyEditor` for adding text to a signed email

---
+++ Notifications/HTMLBodyEditor.cs
@@ -0,0 +1,30 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+namespace Autogrator.Notifications;
+
+public sealed class HTMLBodyEditor {
+    private readonly IDocument document;
+
+    public HTMLBodyEditor(string body) {
+        IConfiguration config = Configuration.Default.WithDefaultLoader();
+        IBrowsingContext context = new BrowsingContext(config);
+        this.document = context.OpenAsync(request => request.Content(body)).Result;
+    }
+
+    public void AddText(string text) {
+        IElement body = document.QuerySelector("body")!;
+        IElement div = body.QuerySelector("div")!;
+        IElement firstElement = div.FirstElementChild;
+
+        IElement parent = document.CreateElement("p");
+        parent.ClassName = "MsoNormal";
+        firstElement.InsertBefore(parent);
+
+        IElement textElement = document.CreateElement("o:p");
+        textElement.TextContent = text;
+        parent.AppendChild(textElement);
+    }
+
+    public string Content() => document.DocumentElement.OuterHtml;
+}
