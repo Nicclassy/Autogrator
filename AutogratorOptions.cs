757beab5796ba20dc2c41d42e91bc5fdd0c8c0c8
Nicclassy <lucanapoli122@gmail.com>
2025-01-29 22:21:19 +1100

    Add `AutogratorOptions`

---
+++ AutogratorOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Autogrator;
+
+public sealed class AutogratorOptions {
+    public string CopiedFileSuffix { get; set; } = " (Copy)";
+    public string EmailsFolderName { get; set; } = "Emails";
+    public bool OverwriteDownloads { get; set; } = false;
+    public bool UseDefaultLoggingConfiguration { get; set; } = true;
+}
