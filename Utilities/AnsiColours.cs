99f9563492979647a74880ba4abc8146dc47fd0e
Nicclassy <lucanapoli122@gmail.com>
2025-01-30 21:57:03 +1100

    Extend and remodel ANSI codes

--- Utilities/AnsiColours.cs
+++ Utilities/AnsiColours.cs
@@ -1,45 +1,81 @@
-﻿namespace Autogrator.Utilities;
+﻿using System.Globalization;
+
+namespace Autogrator.Utilities;
+
+public interface IAnsiSequence {
+    string ToString();
+}
+
+public readonly record struct AnsiColour(int Code) : IAnsiSequence {
+    public override string ToString() => $"\u001b[{Code}m";
+
+    public static implicit operator string(AnsiColour colour) => colour.ToString();
+
+    public AnsiCombinedColours Combine(params AnsiColour[] colours) =>
+        new(new[] { Code }.Concat(colours.Select(colour => colour.Code)));
+}
+
+public readonly record struct AnsiRgbColour(int R, int G, int B, bool Background = false) : IAnsiSequence {
+    public override string ToString() => $"\u001b[{(Background ? 48 : 38)};2;{R};{G};{B}m";
+
+    public static implicit operator string(AnsiRgbColour colour) => colour.ToString();
+}
+
+public readonly record struct AnsiHexColour(string Hex, bool Background = false) : IAnsiSequence {
+    public override string ToString() {
+        const int mask = 0xFF;
+
+        int value = int.Parse(Hex.TrimStart('#'), NumberStyles.HexNumber);
+        int r = (value >> 16) & mask;
+        int g = (value >> 8) & mask;
+        int b = value & mask;
+        return $"\u001b[{(Background ? 48 : 38)};2;{r};{g};{b}m";
+    }
+
+    public static implicit operator string(AnsiHexColour colour) => colour.ToString();
+}
+
+public readonly record struct AnsiCombinedColours(IEnumerable<int> Codes): IAnsiSequence {
+    public override string ToString() => $"\u001b[{string.Join(';', Codes)}m";
+
+    public static implicit operator string(AnsiCombinedColours colours) => colours.ToString();
+}
 
 public static class AnsiColours {
-    public static readonly string Reset = AnsiColour(0);
-    public static readonly string Black = AnsiColour(30);
-    public static readonly string Red = AnsiColour(31);
-    public static readonly string Green = AnsiColour(32);
-    public static readonly string Yellow = AnsiColour(33);
-    public static readonly string Blue = AnsiColour(34);
-    public static readonly string Magenta = AnsiColour(35);
-    public static readonly string Cyan = AnsiColour(36);
-    public static readonly string White = AnsiColour(37);
+    public static readonly AnsiColour Reset = new(0);
+    public static readonly AnsiColour Black = new(30);
+    public static readonly AnsiColour Red = new(31);
+    public static readonly AnsiColour Green = new(32);
+    public static readonly AnsiColour Yellow = new(33);
+    public static readonly AnsiColour Blue = new(34);
+    public static readonly AnsiColour Magenta = new(35);
+    public static readonly AnsiColour Cyan = new(36);
+    public static readonly AnsiColour White = new(37);
 
-    public static readonly string BgBlack = AnsiColour(40);
-    public static readonly string BgRed = AnsiColour(41);
-    public static readonly string BgGreen = AnsiColour(42);
-    public static readonly string BgYellow = AnsiColour(43);
-    public static readonly string BgBlue = AnsiColour(44);
-    public static readonly string BgMagenta = AnsiColour(45);
-    public static readonly string BgCyan = AnsiColour(46);
-    public static readonly string BgWhite = AnsiColour(47);
+    public static readonly AnsiColour BgBlack = new(40);
+    public static readonly AnsiColour BgRed = new(41);
+    public static readonly AnsiColour BgGreen = new(42);
+    public static readonly AnsiColour BgYellow = new(43);
+    public static readonly AnsiColour BgBlue = new(44);
+    public static readonly AnsiColour BgMagenta = new(45);
+    public static readonly AnsiColour BgCyan = new(46);
+    public static readonly AnsiColour BgWhite = new(47);
 
-    public static readonly string BrightBlack = AnsiColour(90);
-    public static readonly string BrightRed = AnsiColour(91);
-    public static readonly string BrightGreen = AnsiColour(92);
-    public static readonly string BrightYellow = AnsiColour(93);
-    public static readonly string BrightBlue = AnsiColour(94);
-    public static readonly string BrightMagenta = AnsiColour(95);
-    public static readonly string BrightCyan = AnsiColour(96);
-    public static readonly string BrightWhite = AnsiColour(97);
+    public static readonly AnsiColour BrightBlack = new(90);
+    public static readonly AnsiColour BrightRed = new(91);
+    public static readonly AnsiColour BrightGreen = new(92);
+    public static readonly AnsiColour BrightYellow = new(93);
+    public static readonly AnsiColour BrightBlue = new(94);
+    public static readonly AnsiColour BrightMagenta = new(95);
+    public static readonly AnsiColour BrightCyan = new(96);
+    public static readonly AnsiColour BrightWhite = new(97);
 
-    public static readonly string BgBrightBlack = AnsiColour(100);
-    public static readonly string BgBrightRed = AnsiColour(101);
-    public static readonly string BgBrightGreen = AnsiColour(102);
-    public static readonly string BgBrightYellow = AnsiColour(103);
-    public static readonly string BgBrightBlue = AnsiColour(104);
-    public static readonly string BgBrightMagenta = AnsiColour(105);
-    public static readonly string BgBrightCyan = AnsiColour(106);
-    public static readonly string BgBrightWhite = AnsiColour(107);
-
-    public static string AnsiColour(int code) => $"\u001b[{code}m";
-
-    public static string AnsiColour(int r, int g, int b, bool background = false) =>
-        $"\u001b[{(background ? 48 : 38)};2;{r};{g};{b}m";
+    public static readonly AnsiColour BgBrightBlack = new(100);
+    public static readonly AnsiColour BgBrightRed = new(101);
+    public static readonly AnsiColour BgBrightGreen = new(102);
+    public static readonly AnsiColour BgBrightYellow = new(103);
+    public static readonly AnsiColour BgBrightBlue = new(104);
+    public static readonly AnsiColour BgBrightMagenta = new(105);
+    public static readonly AnsiColour BgBrightCyan = new(106);
+    public static readonly AnsiColour BgBrightWhite = new(107);
 }
