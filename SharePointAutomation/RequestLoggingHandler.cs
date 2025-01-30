04cc86e1f24dd91e8bf2ccd9abb231d2e70018a8
Nicclassy <lucanapoli122@gmail.com>
2025-01-30 21:56:43 +1100

    Add more flexibility for request logging

--- SharePointAutomation/RequestLoggingHandler.cs
+++ SharePointAutomation/RequestLoggingHandler.cs
@@ -1,30 +1,41 @@
 ﻿using Microsoft.Extensions.Logging;
 
 using Autogrator.Utilities;
+using Serilog;
 
 namespace Autogrator.SharePointAutomation;
 
 public sealed class RequestLoggingHandler(ILogger<RequestLoggingHandler> logger) : DelegatingHandler(new HttpClientHandler()) {
-    private const bool LoggingEnabled = true;
+    public required bool LoggingEnabled { get; init; }
+    public required bool UseSeparateRequestsLogger { get; init; }
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+        void log(string requestUri, IAnsiSequence ansi) {
+            const string template = "{RequestColour}{Method} {RequestUri}{Reset}";
+            if (UseSeparateRequestsLogger)
+                logger.LogDebug(
+                    template, ansi, request.Method.Method, requestUri, AnsiColours.Reset
+                );
+            else
+                Log.Debug(
+                    template, ansi, request.Method.Method, requestUri, AnsiColours.Reset
+                );
+        }
+        
         if (LoggingEnabled) {
-            string requestColour = RequestColour(request.Method);
+            AnsiColour requestColour = RequestColour(request.Method);
             string requestUri = request
                 .RequestUri!
                 .ToString()
                 .Replace("%2C", ",");
 
-            logger.LogInformation(
-                "{RequestColour}{Method} {RequestUri}{Reset}",
-                requestColour, request.Method.Method, requestUri, AnsiColours.Reset
-            );
+            log(requestUri, requestColour);
         }
             
         return base.SendAsync(request, cancellationToken);
     }
 
-    private static string RequestColour(HttpMethod method) {
+    private static AnsiColour RequestColour(HttpMethod method) {
         if (method == HttpMethod.Get) return AnsiColours.Green;
         if (method == HttpMethod.Post) return AnsiColours.Blue;
         if (method == HttpMethod.Put) return AnsiColours.Yellow;
